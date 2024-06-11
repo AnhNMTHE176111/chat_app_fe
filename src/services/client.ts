623594552f7ad7edb31e7aed931120d88f4583a8
tuananh <anhnmthe176111@fpt.edu.vn>
2024-06-12 01:15:52 +0700

    fix cors

--- src/services/client.ts
+++ src/services/client.ts
@@ -6,6 +6,11 @@ const client = axios.create({
   withCredentials: true,
   timeout: 20000,
   timeoutErrorMessage: "The connection has timed out",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin":
+      "https://tuananh-test.d18jf58l2w76jl.amplifyapp.com",
+  },
 });
 
 client.interceptors.request.use(
